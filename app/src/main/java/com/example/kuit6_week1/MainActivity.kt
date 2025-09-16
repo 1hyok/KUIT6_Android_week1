94657aca5ccf624e5a6f38b60ca03baf5cc89932
Zach <dnfjddk2@gmail.com>
2025-09-16 20:59:22 +0900

    feat: Add SampleRow composable and update MainActivity
    - Add SampleRow composable for arranging content horizontally.
    - Update MainActivity to use SampleRow and display a black box.
    - Add project configuration files for VCS, code style, and device manager.
    - Update Gradle and misc XML files.

--- app/src/main/java/com/example/kuit6_week1/MainActivity.kt
+++ app/src/main/java/com/example/kuit6_week1/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kuit6_week1.ui.theme.Kuit6_week1Theme
 
@@ -19,29 +20,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Kuit6_week1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Sample(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()) {
+        SampleRow {
+            Box(
+                Modifier
+                    .background(color = Color.Black)
+                    .fillMaxSize()
+            ) {
+
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    Kuit6_week1Theme {
-        Greeting("Android")
-    }
+private fun SamplePreview() {
+    Sample()
 }
